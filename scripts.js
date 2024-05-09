cfc7db60a29eb42f0e0f249a93bc4a29e3b4bfa4
Lugan2004 <visagielugan42@gmail.com>
2024-05-09 12:30:28 +0200

    succesfully used eslint and prettier

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-const form = document.querySelector("[data-form]");
-const result = document.querySelector("[data-result]");
+const form = document.querySelector('[data-form]');
+const result = document.querySelector('[data-result]');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
